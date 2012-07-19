4218f5c93919457513d8e49f44d718bef74dc420
nonuts <benjamin@nonuts.local>
2012-07-19 15:31:31 +0200

    bootstrap and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Nimblepieces::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+  root :to => 'static_pages#home'
+  
+  match '/signup', :to => 'static_pages#signup'
+  
+  match '/help', :to => 'static_pages#help'
 
+  match '/contact', :to => 'static_pages#contact'
+  
   resources :nimbles
 
   resources :artists
