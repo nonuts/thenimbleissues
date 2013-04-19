b41e543f830a1b9f2e51e5993498f48dde9afe8f
benjaminrabe <benjamin@nonuts.de>
2013-04-19 09:50:10 +0200

    removed all traces of laker

--- build/javascripts/app.js
+++ build/javascripts/app.js
@@ -1,11 +1,4 @@
 (function() {
-  var str_replace;
-
-  str_replace = function(haystack, needle, replacement) {
-    var temp;
-    temp = haystack.split(needle);
-    return temp.join(replacement);
-  };
 
   window.addEventListener("focus", (function() {
     return $("#logo-title #logo").addClass("animated tada");
@@ -15,37 +8,5 @@
     $("#logo-title #logo").removeClass("animated tada");
     return $("#html5-video")[0].pause();
   }), true);
-
-  $(document).ready(function() {
-    var elm, max, min, reset, size;
-    min = 9;
-    max = 26;
-    reset = $(".resizable p").css("fontSize");
-    elm = $(".resizable p");
-    size = str_replace(reset, "px", "");
-    $("a.fontSizePlus").click(function() {
-      if (size <= max) {
-        size++;
-        elm.css({
-          fontSize: size
-        });
-      }
-      return false;
-    });
-    $("a.fontSizeMinus").click(function() {
-      if (size >= min) {
-        size--;
-        elm.css({
-          fontSize: size
-        });
-      }
-      return false;
-    });
-    return $("a.fontReset").click(function() {
-      return elm.css({
-        fontSize: reset
-      });
-    });
-  });
 
 }).call(this);
--- source/javascripts/app.coffee
+++ source/javascripts/app.coffee
@@ -1,6 +1,3 @@
-str_replace = (haystack, needle, replacement) ->
-  temp = haystack.split(needle)
-  temp.join replacement
 window.addEventListener "focus", (->
   $("#logo-title #logo").addClass "animated tada"
 ), true
@@ -8,24 +5,4 @@ window.addEventListener "blur", (->
   $("#logo-title #logo").removeClass "animated tada"
   $("#html5-video")[0].pause()
 ), true
-$(document).ready ->
-  min = 9
-  max = 26
-  reset = $(".resizable p").css("fontSize")
-  elm = $(".resizable p")
-  size = str_replace(reset, "px", "")
-  $("a.fontSizePlus").click ->
-    if size <= max
-      size++
-      elm.css fontSize: size
-    false
-
-  $("a.fontSizeMinus").click ->
-    if size >= min
-      size--
-      elm.css fontSize: size
-    false
-
-  $("a.fontReset").click ->
-    elm.css fontSize: reset
 
